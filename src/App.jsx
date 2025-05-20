ca024770469f1ef084359f93f4f9e8a17ad4f329
allimamalar <allimamalararivudainambi@gmail.com>
2025-05-20 09:40:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react" 
+import { Sample } from "./component/Sample";
+
+function App() {
+  //const[name,setName]=useState("")
+  //console.log(name);
+  
+  const myName="malarandiiii"
+  const myAge=20
+  const myNative="iluppur"
+  const[count,setCount]=useState(0)
+  return (
+    <>
+
+    <h1>My React App</h1>
+    <h1 className="text-30xl font-bold underline text-blue-600"> HELLO WORLD </h1>
+   {/*} <Sample myName={myName} myAge={myAge} myNative={myNative}/>*/}
+    <button className="bg-amber-400 p-2"onClick={()=>setCount(count+1)}>click karo {count}</button>
+    <Sample count={count}/>
+  </>
+  )
+}
+
+export default App
---
+++ src/component/Sample.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Sample = ({count}) => {
+  return (
+    <div>
+
+       {/* vanga palagalam by {myName}
+        <p>malaroda vayasu ipo {myAge}</p>
+        <p>malaroda ooru {myNative}</p>*/}
+        <p>Current count:{count}</p>
+
+    </div>
+  )
+}
+export default Sample
